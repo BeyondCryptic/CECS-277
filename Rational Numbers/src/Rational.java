2012507f8381fe49dc5bb85202e4ddb2b0f6b441
David Van <David@DavidVan.net>
2015-01-26 15:50:35 -0800

    Finished project, I think.

--- Rational Numbers/src/Rational.java
+++ Rational Numbers/src/Rational.java
@@ -36,7 +36,8 @@ public class Rational {
 	}
 	
 	public String toString() {
-		return getNumerator() + "\n-\n" + getDenominator();
+		return getNumerator() + "/" + getDenominator();
+		//return getNumerator() + "\n-\n" + getDenominator();
 	}
 	
 	private int gcd(int m, int n) {
@@ -50,7 +51,61 @@ public class Rational {
 	}
 	
 	public Rational add(Rational f) {
-		//f.getNumerator();
+		Rational newNumber = new Rational();
+		int fNumerator = f.getNumerator();
+		int fDenominator = f.getDenominator();
+		int finalNumerator = this.getNumerator()*fDenominator+this.getDenominator()*fNumerator;
+		int finalDenominator = this.getDenominator()*fDenominator;
+		int scalar = gcd(finalNumerator, finalDenominator);
+		newNumber.setNumerator(finalNumerator/scalar);
+		newNumber.setDenominator(finalDenominator/scalar);
+		return newNumber;
+	}
+	
+	public void sub(Rational f1, Rational f2) {
+		Rational newNumber = new Rational();
+		int f1Numerator = f1.getNumerator();
+		int f1Denominator = f1.getDenominator();
+		int f2Numerator = f2.getNumerator();
+		int f2Denominator = f2.getDenominator();
+		int finalNumerator = f1Numerator*f2Denominator-f1Denominator*f2Numerator;
+		int finalDenominator = f1Denominator*f2Denominator;
+		int scalar = gcd(finalNumerator, finalDenominator);
+		if (scalar < 0) {
+			scalar = scalar*-1;
+		}
+		newNumber.setNumerator(finalNumerator/scalar);
+		newNumber.setDenominator(finalDenominator/scalar);
+		System.out.println(f1.toString() + " - " + f2.toString() + " = " + newNumber.toString());
+	}
+	
+	public Rational mul(Rational f) {
+		Rational newNumber = new Rational();
+		int fNumerator = f.getNumerator();
+		int fDenominator = f.getDenominator();
+		int finalNumerator = this.getNumerator()*fNumerator;
+		int finalDenominator = this.getDenominator()*fDenominator;
+		int scalar = gcd(finalNumerator, finalDenominator);
+		newNumber.setNumerator(finalNumerator/scalar);
+		newNumber.setDenominator(finalDenominator/scalar);
+		return newNumber;
+	}
+	
+	public void div(Rational f1, Rational f2) {
+		Rational newNumber = new Rational();
+		int f1Numerator = f1.getNumerator();
+		int f1Denominator = f1.getDenominator();
+		int f2Numerator = f2.getDenominator(); // Flipped to divide
+		int f2Denominator = f2.getNumerator(); // Flipped to divide
+		int finalNumerator = f1Numerator*f2Numerator;
+		int finalDenominator = f1Denominator*f2Denominator;
+		int scalar = gcd(finalNumerator, finalDenominator);
+		if (scalar < 0) {
+			scalar = scalar*-1;
+		}
+		newNumber.setNumerator(finalNumerator/scalar);
+		newNumber.setDenominator(finalDenominator/scalar);
+		System.out.println(f1.toString() + " / " + f2.toString() + " = " + newNumber.toString());
 	}
 	
 }
--- Rational Numbers/src/RationalNumbers.java
+++ Rational Numbers/src/RationalNumbers.java
@@ -1,8 +1,49 @@
 
 public class RationalNumbers {
+	
+	public static Rational myRational;
+	public static Rational myRational2;
+	
 	public static void main(String[] args) {
-		Rational myRational = new Rational();
+		myRational = new Rational();
+		myRational2 = new Rational();
+		
 		myRational.inputRational();
+		System.out.println("The first number is: ");
 		System.out.println(myRational.toString());
+		myRational2.inputRational();
+		System.out.println("The second number is: ");
+		System.out.println(myRational2.toString());
+		
+		System.out.println("The numbers added is: ");
+		System.out.print(myRational.toString() + " + ");
+		System.out.print(myRational2.toString() + " = ");
+		System.out.println(myRational.add(myRational2).toString());
+		
+		System.out.println("The numbers subtracted is: ");
+		myRational.sub(myRational, myRational2);
+		
+		System.out.println("The numbers multiplied is: ");
+		System.out.print(myRational.toString() + " * ");
+		System.out.print(myRational2.toString() + " = ");
+		System.out.println(myRational.mul(myRational2).toString());
+		
+		System.out.println("The numbers divided is: ");
+		myRational.div(myRational, myRational2);
+		
+		System.out.println(Rational(myRational, myRational2));
+		
+		myRational.setNumerator(2);
+		myRational2.setDenominator(5);
+		
+		System.out.println("Numerator: " + myRational.getNumerator());
+		System.out.println("Denominator: " + myRational2.getDenominator());
+		
+	}
+	
+	public static double Rational(Rational f1, Rational f2) {
+		double decimalAnswer;
+		decimalAnswer = (double)(f1.getNumerator()*f2.getDenominator())/(double)(f1.getDenominator()*f2.getNumerator());
+		return decimalAnswer;
 	}
 }
