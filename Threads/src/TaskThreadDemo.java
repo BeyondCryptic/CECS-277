30c84b21fe8e59d57a73d75dedf296a29dfb425e
David Van <David@DavidVan.net>
2015-05-04 00:41:56 -0700

    Fixed another bug.

--- Threads/src/TaskThreadDemo.java
+++ Threads/src/TaskThreadDemo.java
@@ -3,25 +3,19 @@ import java.util.concurrent.*;
 
 public class TaskThreadDemo {
 	public static void main(String[] args) {
-		TaskThread one = new TaskThread(0);
-		TaskThread two = new TaskThread(1);
-		TaskThread three = new TaskThread(2);
-		TaskThread four = new TaskThread(3);
-		ExecutorService executorOne = Executors.newFixedThreadPool(2);
-		ExecutorService executorTwo = Executors.newFixedThreadPool(2);
-		executorOne.execute(one);
-		executorOne.execute(two);
-		executorOne.shutdown();
-		try {
-			if (executorOne.awaitTermination(2750, TimeUnit.MILLISECONDS)) {
-				// This is just a delay.
-				// Blank so the next few lines always executes!
-			}
-			executorTwo.execute(three);
-			executorTwo.execute(four);
-			executorTwo.shutdown();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
+		Thread one = new Thread(new TaskThread(0));
+		Thread two = new Thread(new TaskThread(1));
+		Thread three = new Thread(new TaskThread(2));
+		Thread four = new Thread(new TaskThread(3));
+		one.start();
+		two.start();
+		while (one.isAlive() && two.isAlive()) {
+			// Does nothing but makes sure only two threads are active.
 		}
+		three.start();
+		while (one.isAlive() || two.isAlive()) {
+			// Does nothing but makes sure only two threads are active.
+		}
+		four.start();
 	}
 }
